69ab7d64238a6c67f7bc24ecdb933ed0928d0fad
Sander Hollaar <sander@humilis.net>
2025-07-02 08:25:54 +0200

    - fix privateKeyHex generation

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cat > local.key <<EOD
 {
   "name": "did:web:statuslist.${BASE_DOMAIN}",
   "type": "ed25519",
-  "privateKeyHex": "$(/usr/local/bin/npm run key ed25519 | grep -A1 'Generating .* private key' | tail -1)"
+  "privateKeyHex": "$(/usr/local/bin/npm run key ed25519 | tail -1)"
 }
 EOD
 
